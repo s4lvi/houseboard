e9a9209cf66318057630a4b6ad939bcd9e35b08b
s4lvi <salvicomputers@gmail.com>
2020-05-03 21:38:33 -0400

    added mongo, implemented post endpoints

--- server/server.py
+++ server/server.py
@@ -1,6 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+from pymongo import MongoClient
+from bson.json_util import dumps
+
+client = MongoClient('localhost', 27017)
+db = client['houseboard']
+posts = db['posts']
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def health():
+    return 'houseboard - server'
+
+@app.route('/<boardId>/', methods=['GET'])
+def getBoard(boardId):
+    return dumps(posts.find())
+
+@app.route('/<boardId>/postMessage', methods=['POST'])
+def postMessage(boardId):
+    posts.insert_one({
+        "message": request.json['message'],
+        "board": boardId
+        })
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run()
